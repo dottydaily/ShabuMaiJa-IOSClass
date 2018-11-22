528a69c8303b378360cd18b1224188eac047b4a2
Pornpat Santibuppakul <dottypurkt@gmail.com>
2018-11-23 04:00:10 +0700

    Re-arrange file into packets

---
+++ ShabuMaiJa-IOSClass/ShabuMaiJa-IOSClass/Controller/LobbyPage/WaitingController.swift
@@ -0,0 +1,31 @@
+//
+//  WaitingController.swift
+//  ShabuMaiJa-IOSClass
+//
+//  Created by DottyPurkt on 23/11/2561 BE.
+//  Copyright © 2561 iOS Dev. All rights reserved.
+//
+
+import UIKit
+
+class WaitingController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.navigationController?.isNavigationBarHidden = true
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
