c524ce3582679e694005fac70f2d456bdeb6a852
Pornpat Santibuppakul <dottypurkt@gmail.com>
2018-11-30 17:13:54 +0700

    Add tableview in each search page (undone)

--- ShabuMaiJa-IOSClass/ShabuMaiJa-IOSClass/Controller/SearchPage/ShowCategoryListController.swift
+++ ShabuMaiJa-IOSClass/ShabuMaiJa-IOSClass/Controller/SearchPage/ShowCategoryListController.swift
@@ -1,31 +1,26 @@
 //
-//  ShowPriceListController.swift
+//  ShowCategoryListController.swift
 //  ShabuMaiJa-IOSClass
 //
-//  Created by DottyPurkt on 30/11/2561 BE.
-//  Copyright © 2561 iOS Dev. All rights reserved.
+//  Created by Pornpat Santibuppakul on 11/30/18.
+//  Copyright © 2018 iOS Dev. All rights reserved.
 //
 
 import UIKit
 
-class ShowPriceListController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ShowCategoryListController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     var accountList: AccountData = AccountData()
     
-    @IBOutlet weak var restaurantTableView: UITableView!
+    @IBOutlet weak var categoryTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         // Do any additional setup after loading the view.
         for _ in 1...20{
             self.accountList.add(account: Account(random: true))
         }
-    }
-    
-    // tap on table view cell
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -37,21 +32,16 @@ class ShowPriceListController: UIViewController, UITableViewDelegate, UITableVie
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "RestaurantByPriceTableViewCell", for: indexPath) as! AccountTableViewCell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "RestaurantByCategoryTableViewCell", for: indexPath) as! AccountTableViewCell
         cell.nameLabel.text = accountList.getAt(index: indexPath.row).name
         cell.usernameLabel.text = accountList.getAt(index: indexPath.row).username
         return cell
     }
     
-    
-
-    
-    
-    
-    
 
     /*
     // MARK: - Navigation
+
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
